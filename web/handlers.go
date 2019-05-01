06a8d0552d2721ccb1a9038ab9ef29500bc4bcca
Grant Limberg <grant.limberg@zerotier.com>
2019-05-01 10:33:09 -0700

    set custom frame options

--- web/handlers.go
+++ web/handlers.go
@@ -83,10 +83,12 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	if h.IsStatic {
 		// Instruct the browser not to display us in an iframe unless is the same origin for anti-clickjacking
-		w.Header().Set("X-Frame-Options", "SAMEORIGIN")
+		// w.Header().Set("X-Frame-Options", "SAMEORIGIN")
+		w.Header().Set("X-Frame-Options", "ALLOW FROM https://my.zerotier.com")
 		// Set content security policy. This is also specified in the root.html of the webapp in a meta tag.
 		w.Header().Set("Content-Security-Policy", fmt.Sprintf(
-			"frame-ancestors 'self'; script-src 'self' cdn.segment.com/analytics.js/%s",
+			"frame-ancestors 'self' %s; script-src 'self' cdn.segment.com/analytics.js/%s",
+			"https://my.zerotier.com",
 			h.cspShaDirective,
 		))
 	} else {
