7b3c62c7a6a381753c5e2ba8a176c42cd72677f0
Grant Limberg <grant.limberg@zerotier.com>
2019-05-02 13:44:21 -0700

    whoops

--- web/handlers.go
+++ web/handlers.go
@@ -85,7 +85,7 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		// Instruct the browser not to display us in an iframe unless is the same origin for anti-clickjacking
 		// w.Header().Set("X-Frame-Options", "SAMEORIGIN")
 		w.Header().Set("X-Frame-Options", "ALLOW FROM https://my.zerotier.com/")
-		Set content security policy. This is also specified in the root.html of the webapp in a meta tag.
+		// Set content security policy. This is also specified in the root.html of the webapp in a meta tag.
 		w.Header().Set("Content-Security-Policy", fmt.Sprintf(
 			"frame-ancestors 'self' %s; script-src 'self' 'unsafe-inline' 'unsafe-eval' cdn.segment.com/analytics.js/%s",
 			"https://my.zerotier.com",
